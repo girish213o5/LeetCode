e62551b86d1a6fe9d269065d4c75fed9d52173b1
Girish M <149922027+girish213o5@users.noreply.github.com>
2025-01-13 19:18:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (76.01%) - LeetHub

---
+++ 2226-rings-and-rods/2226-rings-and-rods.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        int R[10];
+        int G[10];
+        int B[10];
+        int count=0;
+        int l =rings.size();
+        for(int i=0 ; i< l-1 ; i+=2)
+        {
+            if(rings[i]=='B')
+                B[(int)rings[i+1]-'0']=1;
+        
+            if(rings[i]=='R')
+                R[(int)rings[i+1]-'0']=1;
+        
+            if(rings[i]=='G')
+                G[(int)rings[i+1]-'0']=1;
+        }
+        for(int i=0; i<10 ; i++)
+            if(B[i]&R[i]&G[i])
+                ++count;
+
+        return count;
+    }
+};
